627cbaa1efd66776427c33b5826e3c300a7acda6
IrfanHussain2145 <ishussai@purdue.edu>
2025-06-24 18:32:21 -0400

    final image fix

--- src/components/contactMeSection/ContactMeRight.jsx
+++ src/components/contactMeSection/ContactMeRight.jsx
@@ -5,7 +5,7 @@ import ContactSocial from './ContactSocial'
 const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
-        <img src='../public/images/ContactMeImageGen.webp' alt='Contact Me' className='max-w-[300px]'/>
+        <img src='/images/ContactMeImageGen.webp' alt='Contact Me' className='max-w-[300px]'/>
     <ContactInfo />
     <ContactSocial />
     </div>
