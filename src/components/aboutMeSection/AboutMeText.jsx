36d662220f3dfde9c91238f4d4873b60a91305d5
IrfanHussain2145 <ishussai@purdue.edu>
2025-08-17 16:50:50 -0400

    added masters to aboutMe

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -6,11 +6,12 @@ const AboutMeText = () => {
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p className="text-white">
       My name is Irfan Hussain, and I am a Purdue University graduate with 
-      a degree in Computer Science. I am a driven and self-motivated individual 
-      who takes pride in delivering high-quality work and continuously pushing 
-      myself to grow. I thrive on challenges and believe in improving a little every day. 
-      What motivates me most is the opportunity to contribute to meaningful, impactful 
-      projects—especially those that leverage modern technologies to solve real-world problems.
+      a degree in Computer Science. I am currently pursuing my Master’s at the University of 
+      Illinois Urbana-Champaign in Computer Science with a concentration in Data Science. 
+      I am a driven and self-motivated individual who takes pride in delivering high-quality 
+      work and continuously pushing myself to grow. I thrive on challenges and believe in improving 
+      a little every day. What motivates me most is the opportunity to contribute to meaningful, impactful 
+      projects, especially those that leverage modern technologies to solve real-world problems.
       </p>
       {/* <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan ">
           My Projects
