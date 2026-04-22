709dc3ab80b43ed4cc88ac45c97b77b0b2992a45
Irfan Hussain <90107380+IrfanHussain2145@users.noreply.github.com>
2026-04-22 11:54:34 -0400

    Update AboutMeImage.jsx

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -5,7 +5,7 @@ const AboutMeImage = () => {
       <div className="h-[500px] w-[300px] relative">
         <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
           <img
-            src="/images/PurdueAboutMe.jpeg"
+            src="/images/MyUniversities.jpg"
             alt="About Me"
             className="h-full w-auto object-cover"
           />
