207ddd947aa838dc7b14e388577c3018e3686cb4
Irfan Hussain <90107380+IrfanHussain2145@users.noreply.github.com>
2026-04-22 11:50:04 -0400

    Revise About Me text for clarity and emphasis
    Updated the About Me section to enhance clarity and focus on analytical skills.

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -5,13 +5,9 @@ const AboutMeText = () => {
     <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
       <h2 className="text-6xl text-cyan mb-10">About Me</h2>
       <p className="text-white">
-      My name is Irfan Hussain, and I am a Purdue University graduate with 
-      a degree in Computer Science. I am currently pursuing my Master’s at the University of 
-      Illinois Urbana-Champaign in Computer Science with a concentration in Data Science. 
-      I am a driven and self-motivated individual who takes pride in delivering high-quality 
-      work and continuously pushing myself to grow. I thrive on challenges and believe in improving 
-      a little every day. What motivates me most is the opportunity to contribute to meaningful, impactful 
-      projects, especially those that leverage modern technologies to solve real-world problems.
+      My name is Irfan Hussain, and I am a Purdue University graduate with a degree in Computer Science. I am currently pursuing my Master’s at the University of Illinois Urbana-Champaign 
+      in Computer Science with a concentration in Data Science. I am an analytical and driven individual who values continuous growth and strives to approach problems with both technical
+      depth and strategic thinking. 
       </p>
       {/* <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center text-white hover:text-cyan ">
           My Projects
