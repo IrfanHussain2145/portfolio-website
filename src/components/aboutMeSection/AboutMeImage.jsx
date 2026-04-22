ef8d36e13243691b06a605502cb94dcf607827ee
Irfan Hussain <90107380+IrfanHussain2145@users.noreply.github.com>
2026-04-22 12:02:55 -0400

    Refactor AboutMeImage component and update image source

--- src/components/aboutMeSection/AboutMeImage.jsx
+++ src/components/aboutMeSection/AboutMeImage.jsx
@@ -1,20 +1,41 @@
+// import React from "react";
+
+// const AboutMeImage = () => {
+//     return (
+//       <div className="h-[500px] w-[300px] relative">
+//         <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
+//           <img
+//               src="/images/myUniversities2.png"
+//               alt="About Me"
+//               className="h-full w-full object-contain"
+//             />
+//         </div>
+//         <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10">
+//         </div>
+//       </div>
+//     );
+//   };
+  
+//   export default AboutMeImage;
+
 import React from "react";
 
 const AboutMeImage = () => {
-    return (
-      <div className="h-[500px] w-[300px] relative">
-        <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
-          <img
-              src="/images/MyUniversities.jpg"
-              alt="About Me"
-              className="h-full w-full object-contain"
-            />
-        </div>
-        <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10">
-        </div>
+  return (
+    <div className="h-[500px] w-[300px] relative">
+      <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
+        <img
+          src="/images/myUniversities2.png"
+          alt="About Me"
+          className="h-full w-full object-cover"
+        />
       </div>
-    );
-  };
-  
-  export default AboutMeImage;
+
+      <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10">
+      </div>
+    </div>
+  );
+};
+
+export default AboutMeImage;
   
