e9bd0a49e41d96b71a6a4c193f3ca9aba3376a4c
Irfan Hussain <90107380+IrfanHussain2145@users.noreply.github.com>
2026-04-23 19:51:33 -0400

    Update resume link in NavbarBtn component

--- src/components/navbar/NavbarBtn.jsx
+++ src/components/navbar/NavbarBtn.jsx
@@ -3,7 +3,7 @@ import { LuArrowDownRight } from "react-icons/lu";
 
 const NavbarBtn = () => {
   return (
-    <a href="/NewResume.pdf" target="_blank" rel="noopener noreferrer"
+    <a href="/IrfanHussain_Resume.pdf" target="_blank" rel="noopener noreferrer"
     className="px-4 py-2 rounded-full text-xl font-bold text-white border-cyan border flex items-center gap-1 bg-gradient-to-r from-cyan to-orange hover:border-orange hover:scale110 transition-all duration-500 hover:shadow-cyanShadow">
         Resume
         <div className="sm:hidden md:block">
